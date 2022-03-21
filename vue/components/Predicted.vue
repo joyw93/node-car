5d829c308ab92a0673dc796aa86666edd3045cc0
unknown <joyw93@naver.com>
2022-03-21 12:46:32 +0900

    chart.js 연동

--- vue/components/Predicted.vue
+++ vue/components/Predicted.vue
@@ -1,42 +1,63 @@
 <template>
-  <v-row class="mt-5" justify="center">
-    <v-col cols="4">
-      <div id="text1">조회하신</div>
-      <div id="text2">{{ info.brand }} {{ info.model }}</div>
-      <div id="text3">차량 정보입니다.</div>
-    </v-col>
-    <v-col cols="7">
-      <v-row>
-        <v-col cols="6">
-          <div id="brand">{{ info.brand }}</div>
-          <div id="model">{{ info.model }}</div>
-          <p class="muted">
-            {{ info.age }} &nbsp;|&nbsp; {{ info.odo }}km &nbsp;|&nbsp; {{ info.fuel }}
-          </p>
-          <p>
-            <span>예측가격 &nbsp; </span>
-            <strong id="price">{{ price | comma }}만원</strong>
-          </p>
-          <span class="span">제원정보<v-icon>mdi-chevron-right</v-icon> </span>
-          &nbsp;
-          <span class="span">브랜드정보<v-icon>mdi-chevron-right</v-icon></span>
-        </v-col>
-        <v-col cols="5">
-          <img
-            :src="
-              require(`@/static/images/car/${info.brand}/${info.model}.jpg`)
-            "
-            width="400"
-            height="250"
-          />
-        </v-col>
-      </v-row>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row class="mt-5" justify="center">
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <div id="text1">조회하신</div>
+        <div id="text2">{{ info.brand }} {{ info.model }}</div>
+        <div id="text3">차량 정보입니다.</div>
+      </v-col>
+      <v-col cols="12" sm="12" md="8" lg="8">
+        <v-row>
+          <v-col cols="12" sm="5" md="5" lg="5">
+            <div id="brand">{{ info.brand }}</div>
+            <div id="model">{{ info.model }}</div>
+            <p class="muted">
+              {{ info.age }}년식 &nbsp;|&nbsp; {{ info.odo }}km &nbsp;|&nbsp;
+              {{ info.fuel }}
+            </p>
+            <p>
+              <span>예측가격 &nbsp; </span>
+              <strong id="price">{{ price | comma }}만원</strong>
+            </p>
+            <span class="span"
+              >제원정보<v-icon>mdi-chevron-right</v-icon>
+            </span>
+            &nbsp;
+            <span class="span"
+              >브랜드정보<v-icon>mdi-chevron-right</v-icon></span
+            >
+          </v-col>
+          <v-col cols="12" sm="7" md="7" lg="7">
+            <img
+              :src="
+                require(`@/static/images/car/${info.brand}/${info.model}.jpg`)
+              "
+              width="300"
+              height="250"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row class="mt-5" justify="center">
+      <v-col cols="6" sm="6" md="6" lg="4">
+        <Chart />
+      </v-col>
+      <v-col cols="6" sm="6" md="6" lg="4">
+        <BubbleChart />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import Chart from "~/components/chart/Chart.vue";
+import BubbleChart from "~/components/chart/BubbleChart.vue";
 export default {
+  components: {
+    Chart,
+    BubbleChart,
+  },
   data() {
     return {};
   },
@@ -67,17 +88,17 @@ export default {
 }
 
 #text1 {
-  font-size: 50px;
+  font-size: 48px;
   color: #2196f3;
 }
 
 #text2 {
-  font-size: 50px;
+  font-size: 48px;
   font-weight: bold;
 }
 
 #text3 {
-  font-size: 50px;
+  font-size: 48px;
   color: #757575;
 }
 
---
+++ vue/components/chart/BubbleChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <canvas id="bubbleChartData" width="600" height="300"></canvas>
+  </div>
+</template>
+
+<script>
+import BubbleChart from "chart.js";
+
+export default {
+  data() {
+    return {
+      bubbleChartData: {
+        type: "line",
+        data: {
+          labels: [
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022",
+          ],
+          datasets: [
+            {
+              label: "잔존가치",
+              data: [500, 450, 300, 250, 120, 40, 20, 10],
+              backgroundColor: "rgba(54,73,93,.5)",
+              borderColor: "#36495d",
+              borderWidth: 3,
+            },
+            {
+              label: "주행거리",
+              data: [
+                300,200,100,50,40,30,20,10
+              ],
+              backgroundColor: "rgba(33, 150, 243,.5)",
+              borderColor: "#2196F3",
+              borderWidth: 3,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          lineTension: 1,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  padding: 25,
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+  },
+  mounted() {
+    const ctx = document.getElementById("bubbleChartData");
+    new BubbleChart(ctx, this.bubbleChartData);
+  },
+};
+</script>
