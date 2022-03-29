239d33a6f1c7917be33204f657ec62ce508d0ab3
unknown <joyw93@naver.com>
2022-03-29 12:34:26 +0900

    이미지 업로드

--- vue/components/sell-car/ImageCard.vue
+++ vue/components/sell-car/ImageCard.vue
@@ -6,9 +6,29 @@
       <h3 id="text1">
         번호판과 계기판 주행거리가 잘 보이는지 꼭 확인해주세요.
       </h3>
-      <h2>※사진등록(최대 30장, 필수3장 / 1장당 5MB 이하)</h2>
+      <h2>※사진등록(최대 6장 / 1장당 5MB 이하)</h2>
       <h3 id="text2">많은 사진을 등록할수록 정확한 견적에 용이합니다.</h3>
       <v-row>
+        <v-col cols="4">
+          <v-card
+            class="image-card"
+            width="180"
+            height="130"
+            @click="onClickImageUpload"
+          >
+            <input
+              ref="imageInput"
+              type="file"
+              multiple
+              hidden
+              @change="onChangeImages"
+            />
+            <template v-if="imageUrl">
+              <img :src="imageUrl" class="image" />
+            </template>
+            <template v-else> <v-icon x-large> mdi-plus </v-icon> </template>
+          </v-card>
+        </v-col>
         <v-col cols="4">
           <v-card
             class="image-card"
@@ -34,13 +54,18 @@
             <v-icon x-large> mdi-plus </v-icon></v-card
           >
         </v-col>
+      </v-row>
+      <v-row>
         <v-col cols="4">
           <v-card class="image-card" width="180" height="130">
             <v-icon x-large> mdi-plus </v-icon></v-card
           >
         </v-col>
-      </v-row>
-      <v-row>
+        <v-col cols="4">
+          <v-card class="image-card" width="180" height="130">
+            <v-icon x-large> mdi-plus </v-icon></v-card
+          >
+        </v-col>
         <v-col cols="4">
           <v-card class="image-card" width="180" height="130">
             <v-icon x-large> mdi-plus </v-icon></v-card
@@ -56,6 +81,7 @@ export default {
   data() {
     return {
       imageUrl: "",
+      imageUrls: []
     };
   },
   computed: {},
@@ -65,9 +91,8 @@ export default {
     },
     onChangeImages(e) {
       const image = e.target.files;
-      console.log(image);
-      const data = URL.createObjectURL(image[0]);
-      this.imageUrl = data;
+      this.imageUrl = URL.createObjectURL(image[0]);
+      this.imageUrls.push(URL.createObjectURL(image[0]))
     },
   },
 };
@@ -119,6 +144,14 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 span {
