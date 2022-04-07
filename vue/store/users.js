d2c45d9e86ea51f8cf5d8447fa4a9c427b470f9f
JoYongWon <joyw93@naver.com>
2022-04-07 13:52:13 +0900

    배포

--- vue/store/users.js
+++ vue/store/users.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const url =
   process.env.NODE_ENV === "production"
-    ? "http://3.39.63.126"
+    ? "http://nodecar-api.co.kr"
     : "http://127.0.0.1:3080";
 
 export const state = () => ({
