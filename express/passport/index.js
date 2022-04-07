a1cde6a313de177867dc1214d50a5c50e8fec41e
JoYongWon <joyw93@naver.com>
2022-04-07 12:11:29 +0900

    배포

--- express/passport/index.js
+++ express/passport/index.js
@@ -4,15 +4,13 @@ const User = require("../models/user");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    console.log("시리얼라이즈유저 호출됨")
     done(null, user.id);
   });
 
   passport.deserializeUser(async (id, done) => {
-    console.log("deserialr");
+    console.log("deserializeUser 호출됨");
     try {
       const user = await User.findOne({ where: { id } });
-      console.log("deserializeUser");
       done(null, user);
     } catch (err) {
       console.error(err);
